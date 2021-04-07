c13555117d4c8d9b0d325ed656840f2f8d8b5124
Leesuan <suan0310@naver.com>
2021-04-07 15:17:16 +0900

    change 나무

--- Woong2/src/Woong2/Woong2.java
+++ Woong2/src/Woong2/Woong2.java
@@ -4,7 +4,7 @@ public class Woong2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("안녕 난 소스트리야~");
+		System.out.println("안녕 난 나무트리야~");
 	}
 
 }
